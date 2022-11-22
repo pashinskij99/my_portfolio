efa43f9dcae03a40dd1ee18a6bff6df2d91188d5
pashinskij99 <pashinskij99@icloud.com>
2022-11-22 15:13:01 +0200

    fix transition

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,18 +1,18 @@
 import clsx from 'clsx'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 import 'normalize.css'
-import React, {useEffect, useState} from 'react'
-import {Provider, useDispatch, useSelector} from 'react-redux'
-import {Progress} from '../components/progress'
-import {SideBar} from '../components/sidebar'
-import {Preloader} from '../components/preloader'
-import TransitionPage from '../components/transitionPage';
-import {setIsAnimating} from '../store/progress'
+import React, { useEffect, useState } from 'react'
+import { Provider, useDispatch, useSelector } from 'react-redux'
+import { Progress } from '../components/progress'
+import { SideBar } from '../components/sidebar'
+import { Preloader } from '../components/preloader'
+import TransitionPage from '../components/transitionPage'
+import { setIsAnimating } from '../store/progress'
 import Cursor from '../components/cursor'
-import {store} from '../store/store'
+import { store } from '../store/store'
 import '../styles/index.scss'
 
-const MyApp = ({Component, pageProps}) => {
+const MyApp = ({ Component, pageProps }) => {
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
@@ -21,28 +21,21 @@ const MyApp = ({Component, pageProps}) => {
 
   return (
     <Provider store={store}>
-
       <Preloader isAnimating={!loading} />
 
-      {
-        loading &&
-        (
-            <div className='app'>
+      {loading && (
+        <div className='app'>
+          {window.innerWidth > 767 && <Cursor />}
 
-              {window.innerWidth > 767 && <Cursor />}
+          <Loading />
 
-              <Loading/>
+          <SideBar />
 
-              <SideBar  />
-
-              <TransitionPage>
-                <Component {...pageProps} />
-              </TransitionPage>
-
-            </div>
-          )
-      }
-
+          <TransitionPage>
+            <Component {...pageProps} />
+          </TransitionPage>
+        </div>
+      )}
     </Provider>
   )
 }
@@ -50,13 +43,36 @@ const MyApp = ({Component, pageProps}) => {
 const Loading = () => {
   const router = useRouter()
   const dispatch = useDispatch()
-  const {isAnimatingProgress} = useSelector((state) => state.progress)
+  const { isAnimatingProgress } = useSelector((state) => state.progress)
 
   useEffect(() => {
-    const handleStart = () => dispatch(setIsAnimating(true))
+    const handleFixStyle = () => {
+      const nodes = document.querySelectorAll('link[rel=stylesheet], style:not([media=x])')
+      const copies = [...nodes].map((el) => el.cloneNode(true))
 
+      for (let copy of copies) {
+        copy.removeAttribute('data-n-p')
+        copy.removeAttribute('data-n-href')
+
+        document.head.appendChild(copy)
+      }
+
+      const handler = () => {
+        router.events.off('routeChangeComplete', handler)
+  
+        window.setTimeout(() => {
+          for (let copy of copies) {
+            document.head.removeChild(copy)
+          }
+        }, 1000)
+      }
+
+      router.events.on('routeChangeComplete', handler)
+    }
+    const handleStart = () => dispatch(setIsAnimating(true))
     const handleStop = () => dispatch(setIsAnimating(false))
 
+    router.events.on('beforeHistoryChange', handleFixStyle)
     router.events.on('routeChangeStart', handleStart)
     router.events.on('routeChangeComplete', handleStop)
     router.events.on('routeChangeError', handleStop)
@@ -68,7 +84,7 @@ const Loading = () => {
     }
   }, [router])
 
-  return <Progress isAnimating={isAnimatingProgress}/>
+  return <Progress isAnimating={isAnimatingProgress} />
 }
 
 export default MyApp
